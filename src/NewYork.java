e5f9a715f1dd9bb1aa1d873e34e67e4df9e65552
dhana2221 <dhanatirumalasetti@gmail.com>
2023-06-07 21:40:04 -0400

    Second commit

--- src/NewYork.java
+++ src/NewYork.java
@@ -2,7 +2,8 @@
 public class NewYork {
 
 	public static void main(String[] args) {
-System.out.println("Welcome to NYC");
+    System.out.println("Welcome to NYC");
+    System.out.println("NYC is great!");
 	}
 
 }
